c1952de38b17c0738b5a125d5e2ef2642f69baf7
Joao Gilberto Magalhaes <joao@byjg.com.br>
2018-02-07 20:32:38 -0200

    Fix example;

--- examples/language.php
+++ examples/language.php
@@ -12,29 +12,9 @@ $classifier->train('english', file_get_contents('training_data/language/en.txt')
 $classifier->train('french', file_get_contents('training_data/language/fr.txt'));
 $classifier->train('italy', file_get_contents('training_data/language/it.txt'));
 
-// $classifier->train('en', 'This is english');
-// $classifier->train('fr', 'Je suis Hollandais');
-
 $result = $classifier->classify('il testo che vogliamo classificare.');
 print_r($result);
 $result = $classifier->classify('This is english');
 print_r($result);
 $result = $classifier->classify('Je suis Hollandais');
 print_r($result);
-
-/**
- * @param Classifier $classifier
- * @param $label
- * @param $document
- */
-function trainDocument($classifier, $label, $document)
-{
-    $lines = explode("\n", $document);
-    foreach ($lines as $line) {
-        $line = trim(str_replace("\r", "", $line));
-        if (empty($line) || strpos($line, ";") === 0) {
-            continue;
-        }
-        $classifier->train($label, $line);
-    }
-}
--- examples/sentiment.php
+++ examples/sentiment.php
@@ -11,27 +11,7 @@ $classifier = new Classifier($tokenizer);
 $classifier->train('negative', file_get_contents('training_data/sentiment/negative-words.txt'));
 $classifier->train('positive', file_get_contents('training_data/sentiment/positive-words.txt'));
 
-// $classifier->train('en', 'This is english');
-// $classifier->train('fr', 'Je suis Hollandais');
-
 $result = $classifier->classify('I hate you from the bottom of my heart');
 print_r($result);
 $result = $classifier->classify('I love you from the bottom of my heart');
 print_r($result);
-
-/**
- * @param Classifier $classifier
- * @param $label
- * @param $document
- */
-function trainDocument($classifier, $label, $document)
-{
-    $lines = explode("\n", $document);
-    foreach ($lines as $line) {
-        $line = trim(str_replace("\r", "", $line));
-        if (empty($line) || strpos($line, ";") === 0) {
-            continue;
-        }
-        $classifier->train($label, $line);
-    }
-}
